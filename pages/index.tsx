5e9f16597192668f302641a18f42464b3484348c
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-08 19:43:02 +0200

    :recycle: made sure build works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Stats Preview Card Component</title>
         <meta name="description" content="Stats Preview Card Component" />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
-      <main className={styles.main}>
-        <div className="h-screen w-screen flex items-center justify-center z-10">
+      <main>
+        <div>
           <></>
         </div>
       </main>
