29f68f371a0a1fc871c17f9d35c93dd16da2c4d6
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-08 20:00:39 +0200

    :sparkles: Start on Component

---
+++ components/StatsCard.tsx
@@ -0,0 +1,36 @@
+import styles from "./StatsCard.module.css";
+
+const StatsCard = () => {
+  return (
+    <div className={styles.StatsCardContainer}>
+      <div className={styles.StatsCard}>
+        <div className={styles.StatsCardContent}>
+          <div className={styles.StatsCardContent__leftSide}>
+            <h1>Get insights that help your business grow.</h1>
+            <h2>
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </h2>
+            <div>
+              <div>
+                <span>10k+</span>
+                <span>Companies</span>
+              </div>
+              <div>
+                <span>314</span>
+                <span>Templates</span>
+              </div>
+              <div>
+                <span>12M++</span>
+                <span>Queries</span>
+              </div>
+            </div>
+            <div className={styles.StatsCardContent__rightSide}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatsCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import StatsCard from "../components/StatsCard";
 
 const Home: NextPage = () => {
   return (
@@ -11,9 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div>
-          <></>
-        </div>
+        <StatsCard />
       </main>
 
       <footer />
